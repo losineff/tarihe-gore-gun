1ca084b6cb6392387149b67819cba390d06285e3
losineff <92970535+losineff@users.noreply.github.com>
2021-10-24 16:10:03 +0300

    a

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.button1.Location = new System.Drawing.Point(352, 64);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "göster";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label1.Location = new System.Drawing.Point(-3, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(227, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "öğrenmek istediğiniz gün";
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Location = new System.Drawing.Point(155, 170);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(201, 154);
+            this.listBox1.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.textBox1.Location = new System.Drawing.Point(230, 72);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(86, 27);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label2.Location = new System.Drawing.Point(162, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(204, 31);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ekim ayı günleri";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(506, 439);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt16(textBox1.Text);
+            switch (a)
+            {
+                case 1: listBox1.Items.Add("Pazartesi"); break;
+                case 2: listBox1.Items.Add("Salı"); break;
+                case 3: listBox1.Items.Add("Çarşamba"); break;
+                case 4: listBox1.Items.Add("Perşembe"); break;
+                case 5: listBox1.Items.Add("Cuma"); break;
+                case 6: listBox1.Items.Add("Cumartesi"); break;
+                case 7: listBox1.Items.Add("Pazar"); break;
+                case 8: listBox1.Items.Add("Pazartesi"); break;
+                case 9: listBox1.Items.Add("Salı"); break;
+                case 10: listBox1.Items.Add("Çarşamba"); break;
+                case 11: listBox1.Items.Add("Perşembe"); break;
+                case 12: listBox1.Items.Add("Cuma"); break;
+                case 13: listBox1.Items.Add("Cumartesi"); break;
+                case 14: listBox1.Items.Add("Pazar"); break;
+                case 15: listBox1.Items.Add("Pazartesi"); break;
+                case 16: listBox1.Items.Add("Salı"); break;
+                case 17: listBox1.Items.Add("Çarşamba"); break;
+                case 18: listBox1.Items.Add("Perşembe"); break;
+                case 19: listBox1.Items.Add("Cuma"); break;
+                case 20: listBox1.Items.Add("Cumartesi"); break;
+                case 21: listBox1.Items.Add("Pazar"); break;
+                case 22: listBox1.Items.Add("Pazartesi"); break;
+                case 23: listBox1.Items.Add("Salı"); break;
+                case 24: listBox1.Items.Add("Çarşamba"); break;
+                case 25: listBox1.Items.Add("Perşembe"); break;
+                case 26: listBox1.Items.Add("Cuma"); break;
+                case 27: listBox1.Items.Add("Cumartesi"); break;
+                case 28: listBox1.Items.Add("Pazar"); break;
+                case 29: listBox1.Items.Add("Pazartesi"); break;
+                case 30: listBox1.Items.Add("Salı"); break;
+                case 31: listBox1.Items.Add("Çarşamba"); break;
+                default: listBox1.Items.Add("mevcut tarih giriniz"); break;
+            }
+               
+
+        }
+    }
+}
